d9e793f17b6891188f575e045b55c19257afb4da
TSP-Main <sardarnawaz122@gmail.com>
2026-01-10 00:21:16 +0500

    load bootsrap cdn

--- resources/views/layouts/no-nav.blade.php
+++ resources/views/layouts/no-nav.blade.php
@@ -2,6 +2,8 @@
 <html class="h-100" dir="{{GenericHelper::getSiteDirection()}}" lang="{{session('locale')}}">
 <head>
     @include('template.head')
+    {{-- Bootstrap CDN - Ensure Bootstrap loads on login/register pages --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body class="d-flex flex-column">
 @include('elements.impersonation-header')
@@ -15,6 +17,8 @@
 @include('template.footer-compact',['compact'=>true])
 @include('template.jsVars')
 @include('template.jsAssets')
+{{-- Bootstrap JS CDN - Ensure Bootstrap JS loads on login/register pages --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 @include('elements.language-selector-box')
 </body>
 </html>
